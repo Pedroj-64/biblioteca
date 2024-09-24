4ca05585ae7a2c57b6a855a8aae9a9ed67c1e9e2
Pedroj-64 <pedroj.sotor@uqvirtual.edu.co>
2024-09-23 19:59:27 -0500

    .

--- src/main/java/co/edu/uniquindio/poo/Biblioteca.java
+++ src/main/java/co/edu/uniquindio/poo/Biblioteca.java
@@ -1,10 +1,20 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class Biblioteca {
     private String nombre;
+    private Collection<Libro> listaLibros;
+    private Collection<Prestamo> listaPrestamos;
+    private Collection<Bibliotecario> listaEmpleados;
+
 
     public Biblioteca(String nombre){
         this.nombre=nombre;
+        listaLibros=new LinkedList<>();
+        listaPrestamos=new LinkedList<>();
+        listaEmpleados=new LinkedList<>();
     }
 
     public String getNombre(){
@@ -14,7 +24,36 @@ public class Biblioteca {
     public void setNombre(String nombre){
         this.nombre=nombre;
     }
+    
+    public Collection<Libro> getListaLibros() {
+        return listaLibros;
+    }
 
+    public void setListaLibros(Collection<Libro> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
+
+    public Collection<Prestamo> getListaPrestamos() {
+        return listaPrestamos;
+    }
+
+    public void setListaPrestamos(Collection<Prestamo> listaPrestamos) {
+        this.listaPrestamos = listaPrestamos;
+    }
+
+    public Collection<Bibliotecario> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(Collection<Bibliotecario> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public void agregarLibro(Libro libro){
+        if()
+
+
+    }
     public void consultarLibro(){
 
     }
